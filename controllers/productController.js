13c78aade898085ae7ec94fe3da4cbd899b56a07
nasim67reja <nasimreja67@gmail.com>
2022-09-07 06:10:56 -0700

    Category route added

--- controllers/productController.js
+++ controllers/productController.js
@@ -14,3 +14,34 @@ exports.getProduct = factory.getOne(Product, { path: 'reviews' });
 exports.createProduct = factory.createOne(Product);
 exports.updateProduct = factory.updateOne(Product);
 exports.deleteProduct = factory.deleteOne(Product);
+
+exports.getProductCat = async (req, res) => {
+  try {
+    const category = await Product.aggregate([
+      {
+        $group: {
+          _id: '$categories',
+          numProducts: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        category,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,6 +14,8 @@ router
     productController.getAllProducts
   );
 
+router.route('/product-category').get(productController.getProductCat);
+
 router
   .route('/')
   .get(productController.getAllProducts)
