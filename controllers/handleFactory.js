d73b27b9415d6fa0d8a7fd14f0c4e80b53c806ed
Nasim Reja <nasimreja67@gmail.com>
2022-09-12 11:12:42 +0600

    avoid Conflict

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -4,9 +4,9 @@ const APIFeatures = require('../utlis/apiFeatures');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
-    const tour = await Model.findByIdAndDelete(req.params.id);
+    const doc = await Model.findByIdAndDelete(req.params.id);
 
-    if (!tour) {
+    if (!doc) {
       return next(new AppError('No document found with that ID', 404));
     }
     res.status(204).json({
