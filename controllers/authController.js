2923cb78b5d234c9a7902d5275855fc274502100
nasim67reja <nasimreja67@gmail.com>
2022-09-20 22:44:09 -0700

    Sending Email from api added

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModels');
 const catchAsync = require('../utlis/catchAsync');
 const AppError = require('../utlis/appError');
-const sendEmail = require('../utlis/email');
+const Email = require('../utlis/email');
 
 const signToken = (id) =>
   jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -70,6 +70,10 @@ exports.signup = catchAsync(async (req, res, next) => {
     cart: req.body.cart,
     photo: req.body.photo,
   });
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
 
   createSendToken(newUser, 201, res);
 });
@@ -182,11 +186,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    // await sendEmail({
+    //   email: user.email,
+    //   subject: 'Your password reset token (valid for 10 min)',
+    //   message,
+    // });
 
     res.status(200).json({
       status: 'success',
