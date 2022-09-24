58c8678800bfd783a7b7016c0e4a37171084d017
nasim67reja <nasimreja67@gmail.com>
2022-09-23 20:15:47 -0700

    Finally sendgrid works perfectly

--- utlis/email.js
+++ utlis/email.js
@@ -28,6 +28,7 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const { htmlToText } = require('html-to-text');
+const nodemailerSendgrid = require('nodemailer-sendgrid');
 
 module.exports = class Email {
   constructor(user, url) {
@@ -38,24 +39,30 @@ module.exports = class Email {
   }
 
   newTransport() {
-    // if (process.env.NODE_ENV === 'production') {
-    // Sendgrid
+    if (process.env.NODE_ENV === 'production') {
+      // Sendgrid
+
+      return nodemailer.createTransport(
+        nodemailerSendgrid({
+          apiKey: process.env.SENDGRID_PASSWORD,
+        })
+      );
+      // return nodemailer.createTransport({
+      //   service: 'SendGrid',
+      //   auth: {
+      //     user: process.env.SENDGRID_USERNAME,
+      //     pass: process.env.SENDGRID_PASSWORD,
+      //   },
+      // });
+    }
     return nodemailer.createTransport({
-      service: 'SendGrid',
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
       auth: {
-        user: process.env.SENDGRID_USERNAME,
-        pass: process.env.SENDGRID_PASSWORD,
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
       },
     });
-    // }
-    // return nodemailer.createTransport({
-    //   host: process.env.EMAIL_HOST,
-    //   port: process.env.EMAIL_PORT,
-    //   auth: {
-    //     user: process.env.EMAIL_USERNAME,
-    //     pass: process.env.EMAIL_PASSWORD,
-    //   },
-    // });
   }
 
   // Send the actual email
