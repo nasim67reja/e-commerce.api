45b9e004dc41c685228307ba4e99a54b87e473f5
nasim67reja <nasimreja67@gmail.com>
2022-10-03 22:30:11 -0700

    working on Cors

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const app = express();
 // );
 app.use(
   cors({
-    origin: 'https://nasim67reja.github.io',
+    origin: 'https://nasim67reja.github.io/CareoCIty-ecommerce/',
     optionsSuccessStatus: 200,
   })
 );
