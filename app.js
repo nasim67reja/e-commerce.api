a9abd98327e3504f56a3713de0d2cd4eb3e6f70f
nasim67reja <nasimreja67@gmail.com>
2022-10-04 20:50:24 -0700

    Updated package.json file

--- app.js
+++ app.js
@@ -40,16 +40,16 @@ app.use(
 );
 // app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Credentials', true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-  );
-  next();
-});
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Credentials', true);
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+//   );
+//   next();
+// });
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
