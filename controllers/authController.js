9eb26579260b8c093a6b9b602be1f1424704f467
nasim67reja <nasimreja67@gmail.com>
2022-12-01 13:13:17 +0600

    Small Update on Protect Controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -116,14 +116,15 @@ exports.logout = (req, res) => {
 exports.protect = catchAsync(async (req, res, next) => {
   // 1) Getting token and check if it's there
   let token;
-  if (
+  if (req.cookies.jwt) {
+    token = req.cookies.jwt;
+  } else if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-  } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
   }
+
   if (!token)
     return next(
       new AppError('You are not logged in! Please log in to get access.', 401)
